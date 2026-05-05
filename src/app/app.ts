9d0c6fb285d9694269d6f66266a3f5f8f8b2842a
AntonioPando <antoniogpando@gmail.com>
2026-05-05 18:12:55 +0200

    feat: Add lateral menu component with navigation and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/layout/header/header";
+import { MenuLateral } from "./components/layout/menu-lateral/menu-lateral";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MenuLateral],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
