aff87b442068d5de83730bfd41c934909ed77aca
Pilar Salvador <119239553+pilar098@users.noreply.github.com>
2026-05-06 10:48:00 +0200

    feat: add Perfil component with HTML, CSS, and test files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Movimientos } from './components/pages/movimientos/movimientos';
+import { Perfil } from './components/pages/perfil/perfil';
 
 export const routes: Routes = [
       {
     path: 'movimientos',
     component: Movimientos
+  },
+  { 
+    path: 'perfil',
+    component: Perfil
   }
+
 ];
